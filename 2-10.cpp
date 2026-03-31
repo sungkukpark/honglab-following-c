27abd4180e0743498ee19729063ef942211b8a3b
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-03-31 22:10:12 +0200

    update 2-10 sample code

--- 2-10.cpp
+++ 2-10.cpp
@@ -1,13 +1,19 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main(int argc, char argv[])
 {
-	int n1, n2, n3, n4;
+    int n1, n2, n3, n4;
 
-	n1 = 1;
-	n2 = n1 * n1;
-	n3 = n2 * n1;
-	n4 = n2 * n1;
+    // 문법 오류(Syntax Error)
+    //n5 = 3;
 
-	return 0;
+    n1 = 2;
+    n2 = n1 * n1;
+    n3 = n2 * n1;
+    //n4 = n2 * n1;   // 문맥 오류 (Semantic Error)
+    n4 = n3 * n1;
+
+    printf("%i %i %i %i", n1, n2, n3, n4);
+
+    return 0;
 }
