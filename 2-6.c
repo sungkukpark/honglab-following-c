c0e6a242a1166ab63b2d7c66248e3a03f6baa806
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-03-31 20:42:55 +0200

    add 2-6 sample code

---
+++ 2-6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// printf function
+	//printf("The truth is ...\n I am Ironman.\n");
+
+	int x, y, z;
+
+	x = 3;
+	y = 2;
+
+	z = x + y;
+	printf("%i + %i = %i\n", x, y, x + y);
+
+	printf("\a");
+
+	return 0;
+}
