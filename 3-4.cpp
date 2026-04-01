c337aab541154c9cb61350dfb1798e3e6f5f6d86
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-04-01 19:37:21 +0200

    update 3-4 sample code

--- 3-4.cpp
+++ 3-4.cpp
@@ -2,15 +2,24 @@
 
 int main()
 {
-	int a = 0;
-	int b = 0;
-	int sum = 0;
+	//int a = 0;
+	//int b = 0;
+	//int sum = 0;
 
-	scanf("%d%d", &a, &b);
+	//printf("Input two integers: ");
+	//scanf("%d%d", &a, &b);
 
-	sum = a + b;
+	//sum = a + b;
 
-	printf("%d + %d = %d\n", a, b, sum);
+	//printf("%d + %d = %d\n", a, b, sum);
 
+	float won = 0;
+	float dollar = 0;
+
+	printf("Input won: ");
+	scanf("%f", &won);
+
+	dollar = won * 0.00089f;
+	printf("Dollar = %f\n", dollar);
 	return 0;
 }
