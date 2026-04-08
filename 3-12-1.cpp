20cfe8e1227684f450834239d85f0437f77b098b
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-04-08 12:44:57 +0200

    add 3-12 sample code

---
+++ 3-12-1.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <float.h>
+
+int main(void)
+{
+	// round-off errors (ex1); 반올림 아님
+	float a, b;
+	a = 1.0E20f + 1.0f;	// 앞의 수가 너무 크기 때문에
+	b = a - 1.0E20f;	// 뒤의 숫자가 연산 뒤에 사라지는 문제
+	printf("%f\n", b);
+
+	// round-off errors (ex2); 반올림 아님
+	a = 0.0f;
+	for (int i = 0; i < 100; ++i)
+	{
+		a += 0.01f;		// 실수 연산의 오차 누적이 반복되면 최종 결과값의 오차가 커질 수 있음
+	}
+	printf("%f\n", a);
+
+	// overflow
+	//double max = FLT_MAX;
+	double max = DBL_MAX;
+	printf("%f\n", max);
+
+	max = max * 100.0f;
+	printf("%f\n", max);
+
+	// underflow
+	float f = FLT_TRUE_MIN;
+	printf("%e\n", f);
+
+	f = f / 2.0f;			// subnormal
+	printf("%e\n", f);
+
+	return 0;
+}
---
+++ 3-12-2.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <float.h>
+#include <math.h>
+
+int main(void)
+{
+	//float f = 104.0f;
+	//printf("%f\n", f);
+
+	//f = f / 0.0f;
+	//printf("%f\n", f);
+
+	float f = asinf(1.0f);
+	printf("%f\n", f);
+
+	f = asinf(2.0f);
+	printf("%f\n", f);
+
+	return 0;
+}
