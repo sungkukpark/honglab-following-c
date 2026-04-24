f66d1f179d5ca5e6e231782a1b335d42376710f4
Sungkuk Park <sungkukshawnpark@gmail.com>
2026-04-24 22:43:39 +0200

    add 4-5 code sample

---
+++ chapter_4/4-5.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+
+// 기호적 상수
+#define PI 3.141592f
+#define AI_NAME "Jarvis"
+
+int main()
+{
+	float radius, area, circumference;
+
+	printf("I'm %s.\n", AI_NAME);
+	printf("Please enter the radius of the circle.\n");
+
+	printf("Input radius: \n");
+	scanf("%f", &radius);
+
+	area = PI * radius * radius;	// 원의 넓이: area = π * r²
+	circumference = 2 * PI * radius; // 원의 둘레: 2 * π * r
+	printf("Area is %f\n", area);
+	printf("Circumference is %f\n", circumference);
+
+	// TODO: extra, sphere
+	// sphere area: 4 * π * r²
+	// sphere volume: (4/3) * π * r³
+
+	// TODO: wrong usage, strings, const
+	return 0;
+}
